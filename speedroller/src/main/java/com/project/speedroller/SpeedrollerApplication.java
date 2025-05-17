c2f53fc838d8bbba69c8eea164c0d6f6a503ec1b
Miguel Ángel Arias <84602627+MiguelArias0316@users.noreply.github.com>
2025-05-17 12:08:47 -0500

    first commit

---
+++ speedroller/src/main/java/com/project/speedroller/SpeedrollerApplication.java
@@ -0,0 +1,14 @@
+package com.project.speedroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedrollerApplication.class, args);
+		System.out.println("aplicacion corriendo correctamente");
+	}
+
+}
---
+++ speedroller/src/test/java/com/project/speedroller/SpeedrollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.speedroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedrollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
